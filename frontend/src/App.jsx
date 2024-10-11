3e39aef2913ccea6165fb833b426210257b17d5a
RobertJHog <rj.hogerbrugge@gmail.com>
2024-10-11 14:50:25 +0200

    mogelijke fix topbar import probleem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Topbar from './components/Topbar';
@@ -7,16 +6,13 @@ import Downloads from './pages/Downloads';
 import ServerManagement from './pages/ServerManagement';
 import Games from './pages/Games';
 import Contact from './pages/Contact';
+
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-
   return (
     <div className="h-screen w-screen bg-gray-900 overflow-hidden">
-      <Topbar toggleSidebar={toggleSidebar} />
+      <Topbar />
       <div className="flex flex-1 pt-16">
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} className="w-64" />
+        <Sidebar className="w-64" />
         <main className="flex-1 pl-64">
           <div className="h-full w-full bg-gray-900 p-6">
             <Routes>
