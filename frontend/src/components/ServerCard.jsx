07558ebd469b6a1e0c6626e506ea5d41838bddd9
RobertJHog <rj.hogerbrugge@gmail.com>
2024-10-14 14:13:38 +0200

    add players info to dummy server

--- frontend/src/components/ServerCard.jsx
+++ frontend/src/components/ServerCard.jsx
@@ -1,16 +1,17 @@
 import PropTypes from 'prop-types'
 import { ServerInfo } from './ServerInfo'
+import { GAME_IMAGE_URLS } from '../../../constants/gameImageUrls'
 
 export const ServerCard = ({ server }) => (
   <div className="flex justify-center text-white">
     <div 
       className={`bg-slate-700 shadow-lg rounded-lg overflow-hidden ${server.numplayers && server.numplayers > 0 ? 
         'border-green-500' : 'border-red-500'} border-b-8 w-full max-w-[312px]`}>
-      <div className="w-full h-48">
+      <div className="w-full h-48 overflow-hidden">
         <img 
-          src='/assets/unrealtournament.jpg' 
+          src={GAME_IMAGE_URLS[server.name] || '/assets/no_image.jpeg'}   
           alt={server.name || 'Server image'} 
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover object-center"
         />
       </div>
       <ServerInfo server={server} />
